5bd32fbae103cb5a4c551885205da50b15df93fb
akbarjamal <akbar7jamal@gmail.com>
2017-02-11 16:42:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var img=document.getElementById('crest');
+var margin=0;
+function moveRight()
+{
+    margin+=1;
+    img.style.marginLeft = margin + 'px';
+}
+var onClick = function()
+{
+    var interval = setInterval(moveRight,50);
+}
