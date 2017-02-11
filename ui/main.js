988951c0f6edc373c08c71fa7edea1ed96c92ca8
akbarjamal <akbar7jamal@gmail.com>
2017-02-11 16:50:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,2 @@
 console.log('Loaded!');
-var img=document.getElementById('team');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft= marginLeft + 10;
-    img.style.marginLeft=marginLeft+'px';
-}
-var onClick=function()
-{
-    var interval = setInterval(moveRight,50);
-};
-console.log('Executed!!');
+
