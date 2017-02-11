f7fe38572748647eab842347f89c76cba953ae71
akbarjamal <akbar7jamal@gmail.com>
2017-02-11 16:48:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var img=document.getElementById('team');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft= marginLeft+10;
+    marginLeft= marginLeft + 10;
     img.style.marginLeft=marginLeft+'px';
 }
 var onClick=function()
