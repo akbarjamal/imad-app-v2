fd4f8a7657638c58525c1bcd0f6bb62b15d8aa60
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 13:38:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
 console.log('Loaded!');
-
+var button = document.getElementById('fan');
+var span = document.getElementById('count');
+var counter = 0;
+button.onclick = function() {
+    counter = counter + 1;
+    span.innerHTML = counter.toString();
+};
