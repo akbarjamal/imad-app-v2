98a2c36f7f2969ca08b15599b1721bde554511d8
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 16:58:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,12 @@ console.log('Loaded!');
 var button = document.getElementById('fan');
 button.onclick = function() {
     var request = new XMLHttpRequest();
-    request.onreadystagechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
                 var counter = request.responseText;
-                //console.log(request.responseText);
-                //console.log(request.response);
-                var div = document.getElementById('count');
-                div.innerHTML = counter.toString();
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
             }
         }
     };
@@ -22,3 +20,17 @@ button.onclick = function() {
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };*/
+button.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http://akbarjamal.imad.hasura-app.io/yes', true);
+    request.send(null);
+};
