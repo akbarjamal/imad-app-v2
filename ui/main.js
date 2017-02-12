1eda5ae78f307df6735117d8224067fd8416af96
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 13:50:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,16 @@
 console.log('Loaded!');
 var button = document.getElementById('fan');
 var span = document.getElementById('count');
-var counter = 0;
 button.onclick = function() {
-    counter = counter + 1;
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystagechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var counter = request.responseText;
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
+    request.open('GET','http://akbarjamal.imad.hasura-app.io/yes',true);
+    request.send(null);
 };
