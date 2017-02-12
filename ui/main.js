ae454626278cf8e4e523c9d8baa15bb5de4f962e
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 13:52:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
 console.log('Loaded!');
 var button = document.getElementById('fan');
-var span = document.getElementById('count');
+
 button.onclick = function() {
     var request = new XMLHttpRequest();
     request.onreadystagechange = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
                 var counter = request.responseText;
+                var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
         }
