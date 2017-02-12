25fc5391d9658a6c8c6c139d0d253f964adedd2e
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 11:57:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/yes', function (req,res) {
+  counter = counter + 1;
+  res.send(counter.toString()) ;
+});
+
 app.get('/:year', function (req, res) {
   var year = req.params.year;
   res.send(createTemplate(season[year]));
