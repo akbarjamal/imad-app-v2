79cf477951397011bb7707e78b0a889394338718
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 18:27:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,17 +21,28 @@ button.onclick = function() {
     span.innerHTML = counter.toString();
 };*/
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    var names=['akbar','ameer','abbas'];
-    var list = '';
-    for (var i = 0;i<names.length;i++) {
-        list+='<li>'+ names[i] +'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names=['akbar','ameer','abbas'];
+                var list = '';
+                for (var i = 0;i<names.length;i++) {
+                    list+='<li>'+ names[i] +'</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://akbarjamal.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+
 };
 
 
