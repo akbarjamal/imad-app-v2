c9ef77865afc73a99e783301a89be1ae58045c94
akbarjamal <akbar7jamal@gmail.com>
2017-02-12 18:20:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var names=[];
+app.get('/submit-name', function (req,res) {
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 var season = {
 	'06-07': {
 		title:'Season 06-07 Review:',
@@ -112,13 +119,6 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/ui/crest.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'crest.png'));
-});
-
-var names=[];
-app.get('/submit-name', function (req,res) {
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
