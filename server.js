05766cf8977b6af7ab9008e34eaf7dfb6fe9f8d2
akbarjamal <akbar7jamal@gmail.com>
2017-02-20 23:36:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/yes', function (req, res) {
   res.send(counter.toString()) ;
 });
 
-var names=[];
+var names = [];
 app.get('/submit-name', function (req,res) {
     var name = req.query.name;
     names.push(name);
