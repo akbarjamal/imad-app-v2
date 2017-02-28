72f92583756494b1192ef76ec485b36096bf8650
akbarjamal <akbar7jamal@gmail.com>
2017-02-28 17:15:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,13 +88,13 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 
 app.get('/test', function (req,res) {
-    pool.query('SELECT * FROM test', funcion (err,result) {
+    pool.query('SELECT * FROM test', function (err,result) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
             res.send(JSON.stringify(result.rows));
         }
-    });
+    }); 
 });
 
 var counter = 0;
