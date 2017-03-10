c448d872e2ab12b0d6010da1b1669f76a7d6f607
akbarjamal <akbar7jamal@gmail.com>
2017-03-10 21:44:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,5 +45,29 @@ submit.onclick = function() {
     request.send(null);
 };
 
+var sub = document.getElementById('submit_button');
+sub.onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                alert('User logged in');
+                console.log('Credentials are correct');
+            } else if (request.status === 403) {
+                alert('Wrong username/password');
+            } else if (request.status === 500) {
+                alert('Server down');
+            }
+        }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log('username');
+    console.log('password');
+    request.open('POST', 'http://akbarjamal.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
 
 
